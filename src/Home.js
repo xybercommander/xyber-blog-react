6c134c7a6520fffd80639b98ecc661e7afc59b5a
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-03-18 20:53:14 +0530

    implemented isPending(Loading...)

--- src/Home.js
+++ src/Home.js
@@ -4,21 +4,26 @@ import BlogList from './BlogList';
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);  
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                // console.log(data);
-                setBlogs(data);
-            });
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    return res.json()
+                })
+                .then(data => {
+                    // console.log(data);
+                    setBlogs(data);
+                    setIsPending(false);
+                });
+        }, 1000);
     }, []);
 
     // Main component
     return ( 
         <div className="home">            
+            { isPending && <div>Loading...</div> }
             {blogs && <BlogList blogs={blogs} title={'All Blogs!'} />}
             {/* If the blogs value is null then BlogList component wont even render */}
         </div>
