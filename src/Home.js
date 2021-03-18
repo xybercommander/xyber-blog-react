bb32268b8ae675c2aa68bf9d7f97fc678df71385
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-03-18 21:07:19 +0530

    Handled errors

--- src/Home.js
+++ src/Home.js
@@ -5,24 +5,34 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);  
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
                 .then(res => {
+                    if(!res.ok) {
+                        throw Error('could not fetch data for the resource');
+                    }
                     return res.json()
                 })
                 .then(data => {
                     // console.log(data);
                     setBlogs(data);
                     setIsPending(false);
+                    setError(null);
+                })
+                .catch((err) => {
+                    setIsPending(false);
+                    setError(err.message);
                 });
         }, 1000);
     }, []);
 
     // Main component
     return ( 
-        <div className="home">            
+        <div className="home">
+            { error && <div>{ error }</div> }
             { isPending && <div>Loading...</div> }
             {blogs && <BlogList blogs={blogs} title={'All Blogs!'} />}
             {/* If the blogs value is null then BlogList component wont even render */}
