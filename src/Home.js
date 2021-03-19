281b57c684bad3b6c3e2714012e7962115ed17d9
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-03-19 19:17:53 +0530

    made a custom hook(useFetch())

--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,18 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
-const Home = () => {
+const Home = () => {    
 
-    const [blogs, setBlogs] = useState(null);  
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if(!res.ok) {
-                        throw Error('could not fetch data for the resource');
-                    }
-                    return res.json()
-                })
-                .then(data => {
-                    // console.log(data);
-                    setBlogs(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch((err) => {
-                    setIsPending(false);
-                    setError(err.message);
-                });
-        }, 1000);
-    }, []);
+    // Calling the custom hook 'useFetch' that we made
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
     // Main component
     return ( 
         <div className="home">
             { error && <div>{ error }</div> }
             { isPending && <div>Loading...</div> }
-            {blogs && <BlogList blogs={blogs} title={'All Blogs!'} />}
+            { blogs && <BlogList blogs={blogs} title={'All Blogs!'} /> }
             {/* If the blogs value is null then BlogList component wont even render */}
         </div>
     );
