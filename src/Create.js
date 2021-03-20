708ce5002d096876d294c6d6b0ae1c78b9ae8afc
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-03-20 08:53:47 +0530

    made the forms

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
 const Create = () => {
+   const [title, setTitle] = useState('');
+   const [body, setBody] = useState('');
+   const [author, setAuthor] = useState('Xyber');
+
    return ( 
       <div className="create">
          <h2>Add a New Blog</h2>
+         <form>
+            <label>Blog title:</label>
+            <input 
+               type="text" 
+               required
+               value={title}
+               onChange={(e) => setTitle(e.target.value)}
+            />
+            <label>Blog body:</label>
+            <textarea 
+               required
+               value={body}
+               onChange={(e) => setBody(e.target.value)}
+            ></textarea>
+            <label>Blog author:</label>
+            <select
+               value={author}
+               onChange={(e) => setAuthor(e.target.value)}
+            >
+               <option value="Xyber">Xyber</option>
+               <option value="Samrat">Samrat</option>
+            </select>
+            <button>Add Blog</button>            
+         </form>         
       </div>
    );
 }
